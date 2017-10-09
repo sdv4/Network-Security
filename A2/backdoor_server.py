b5301b2b2e85747c6e76be711a59b5efdab01a4e
shanesims <shane.sims.ss@gmail.com>
2017-10-09 10:56:22 -0600

    added prompt for client password

--- A2/backdoor_server.py
+++ A2/backdoor_server.py
@@ -13,25 +13,37 @@
 import socketserver
 import socket, threading
 import sys
+
+server_password = "thepasswordfornow"                                           # Hard coded server password
+
 class MyTCPHandler(socketserver.BaseRequestHandler):                            # Create a request handler as subclass of BaseRequestHandler
    BUFFER_SIZE = 4096                                                           # Input buffer for client requests will be 4096 bytes
+   PASSWORD_SIZE = 1024
    def handle(self):                                                            # Override parent class handle method to handle incoming requests
-       while 1:                                                                 # Keep taking requests from a connected client without dropping connection
-           data = self.request.recv(self.BUFFER_SIZE)                           # self.request is the TCP socket connected to the client and recv stores command from client into data
-           if len(data) == self.BUFFER_SIZE:
-               while 1:
-                   try:  # error means no more data
-                       data += self.request.recv(self.BUFFER_SIZE, socket.MSG_DONTWAIT)
-                   except:
-                       break
-           if len(data) == 0:
-               break
-           data = data.decode( "utf-8")
-           self.request.sendall( bytearray( "You said: " + data, "utf-8"))
-           print("%s (%s) wrote: %s" % (self.client_address[0],
-                 threading.currentThread().getName(), data.strip()))
+       self.request.sendall( bytearray( "Enter Password: ", "utf-8"))
+       client_password = self.request.recv(self.PASSWORD_SIZE)
+       client_password = client_password.decode("utf-8")
+       client_password = client_password.strip()
+       if client_password == server_password:
+           self.request.sendall( bytearray( "Password correct. Welcome.\n", "utf-8"))
+           while 1:
+               data = self.request.recv(self.BUFFER_SIZE)                           # self.request is the TCP socket connected to the client and recv stores command from client into data
+               if len(data) == self.BUFFER_SIZE:
+                   while 1:
+                       try:  # error means no more data
+                           data += self.request.recv(self.BUFFER_SIZE, socket.MSG_DONTWAIT)
+                       except:
+                           break
+               if len(data) == 0:
+                   break
+               data = data.decode( "utf-8")
+               self.request.sendall( bytearray( "You said: " + data, "utf-8"))
+               print("%s (%s) wrote: %s" % (self.client_address[0],
+                    threading.currentThread().getName(), data.strip()))
+       else:
+           self.request.sendall( bytearray( "Incorrect Password. Goodbye.\n", "utf-8"))
 
 if __name__ == "__main__":
    HOST, PORT = "localhost", int(sys.argv[1])                                   # Get port number from command line
-   server = socketserver.ThreadingTCPServer((HOST, PORT), MyTCPHandler)         # Instantiate the threaded TCP server classs and give it our handler
+   server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)                  # Instantiate the threaded TCP server class and give it our handler
    server.serve_forever()                                                       # Instruct server to handle many requests
