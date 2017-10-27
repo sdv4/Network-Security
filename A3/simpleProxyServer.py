6c73cfa113a66d44b2021e6a43d5bab639d3583e
shanesims <shane.sims.ss@gmail.com>
2017-10-27 16:56:37 -0600

    fixed output stdout, implemented -raw

--- A3/simpleProxyServer.py
+++ A3/simpleProxyServer.py
@@ -3,15 +3,33 @@
 # https://docs.python.org/2/tutorial/datastructures.html
 # Select example given in tutorial
 
+#TODO: add keyboard interupt
+#TODO: figure out if when how to close the sockets and return the "Connection closed" message
+
+import string
 import socket
 import sys
 import datetime
 import select
-from threading import Thread
 
 global errorString
 errorString = ("Usage: python3 proxyServer.py [logOptions] [replaceOptions]" +
         "<source port> <server> <destination port>")
+
+print(string.printable)
+def removeNonPrintable(lines):
+    oldLines = lines
+    newLines = []
+    for line in oldLines:
+        print(str(line))
+        newln = ''
+        for c in line:
+            if str(c) in string.printable:
+                newln = newln + " " + str(c)
+            else:
+                newln = newln + '.'
+        newLines.append(newln)
+    return newLines
 
 
 if __name__ == "__main__":
@@ -21,62 +39,63 @@ if __name__ == "__main__":
     global loggingOption
     global replaceOptions
 
-    if len(sys.argv) == 4:
+    cmdLineArgs = len(sys.argv)
+    loggingOption  = ""
+    replaceOptions = ""
+    if cmdLineArgs == 4:
         HOST, srcPort = "localhost", int(sys.argv[1])                           # Get port number to listen on as command line input
         destServer, destPort = str(sys.argv[2]), int(sys.argv[3])               # Get destination server name or IP and port number to connect to
+    elif cmdLineArgs == 5:
+        loggingOption = str(sys.argv[1])
+        print(loggingOption)
+        HOST, srcPort = "localhost", int(sys.argv[2])                           # Get port number to listen on as command line input
+        destServer, destPort = str(sys.argv[3]), int(sys.argv[4])               # Get destination server name or IP and port number to connect to
+    elif cmdLineArgs == 6:
+        loggingOption, replaceOptions = str(sys.argv[1]), str(sys.argv[2])
+        HOST, srcPort = "localhost", int(sys.argv[3])                           # Get port number to listen on as command line input
+        destServer, destPort = str(sys.argv[4]), int(sys.argv[5])               # Get destination server name or IP and port number to connect to
+    else:                                                                       # args must be less than 4 or greater than 6
+        print(errorString)
+        sys.exit()
+    proxyListeningSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    proxyListeningSocket.setblocking(0)                                         # Make socket non binding so that more than one connection can be made
+    proxyListeningSocket.bind((HOST, srcPort))
+    proxyListeningSocket.listen(5)                                              # Listen for connections and queue up to 5 of them
+    print("Port logger running: srcPort=" + str(srcPort) + " host=" + destServer + " dstPort=" + str(destPort) + '\n')
 
-    #sproxyListeningSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        proxyListeningSocket = socket.socket()
-        proxyListeningSocket.setblocking(0)                                     # Make socket non binding so that more than one connection can be made
-        proxyListeningSocket.bind((HOST, srcPort))
-        proxyListeningSocket.listen(5)                                          # Listen for connections and queue up to 5 of them
-        print("Port logger running: srcPort=" + str(srcPort) + " host=" + destServer + " dstPort=" + str(destPort))
+    potential_readers = [proxyListeningSocket]
+    potential_writers = []
+    potential_errors = []
 
-        potential_readers = [proxyListeningSocket]
-        potential_writers = []
-        potential_errors = []
-
-        dictionaryOfWriters = {}                                                # will hold pairs of (A, B) where A writes only to B
-
-
-        while potential_readers:
-            ready_to_read, ready_to_write, in_error = select.select(potential_readers, potential_writers, potential_errors)
-            for sock in ready_to_read: #for each sock that has something to ready_to_read
-                if sock is proxyListeningSocket: #then the thing being read is going to be a new local client conntection, which will need its own server connection
-                    remoteServerConn = socket.socket()                          # set up remote server connection for incoming client
-                    remoteServerConn.connect((destServer,destPort))
-                    localClientConn, clientAddress = sock.accept()              # accept incoming local client
-                    localClientConn.setblocking(0)                              # double check that connection in non binding so more clients can connect
-                    currentDateTime = datetime.datetime.now()
-                    print("New connection: " + str(currentDateTime) + ", from " + str(clientAddress) + "\n" )
-                    potential_readers.append(localClientConn)                   # add connection to those that may have something to read
-                    potential_readers.append(remoteServerConn)
-                    dictionaryOfWriters[localClientConn] = remoteServerConn     #now if localClientConn is ready_to_read, it will only do so from remoteServerConn
-                    dictionaryOfWriters[remoteServerConn] = localClientConn     #and if remoteServerConn is ready_to_read, it will only do so from localClientConn
-                    break
+    dictionaryOfWriters = {}                                                    # will hold pairs of (A, B) where A writes only to B
+    dictionaryOfClientWriters ={}
+    while potential_readers:
+        ready_to_read, ready_to_write, in_error = select.select(potential_readers, potential_writers, potential_errors)
+        for sock in ready_to_read: #for each sock that has something to ready_to_read
+            if sock is proxyListeningSocket: #then the thing being read is going to be a new local client conntection, which will need its own server connection
+                remoteServerConn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                              # set up remote server connection for incoming client
+                remoteServerConn.connect((destServer,destPort))
+                localClientConn, clientAddress = sock.accept()                  # accept incoming local client
+                localClientConn.setblocking(0)                                  # double check that connection in non binding so more clients can connect
+                currentDateTime = datetime.datetime.now()
+                print("New connection: " + str(currentDateTime) + ", from " + str(clientAddress) + "\n" )
+                potential_readers.append(localClientConn)                       # add connection to those that may have something to read
+                potential_readers.append(remoteServerConn)
+                dictionaryOfClientWriters[localClientConn] = remoteServerConn         #now if localClientConn is ready_to_read, it will only do so from remoteServerConn
+                dictionaryOfWriters[remoteServerConn] = localClientConn         #and if remoteServerConn is ready_to_read, it will only do so from localClientConn
+            else:
                 dataFromSock = sock.recv(1024)                                  # get data in 1024 byte chunks
                 if dataFromSock: #i.e. not empty
                     linesOfData = dataFromSock.split(b'\n')
-                    if sock is remoteServerConn:
-                        for line in linesOfData:
-                            print("<--- %s" % line)
+                    if loggingOption == "-strip":
+                        linesOfData = removeNonPrintable(linesOfData)
+                    if sock in dictionaryOfWriters:
+                        if loggingOption == "-raw" or loggingOption == "-strip":
+                            for line in linesOfData:
+                                sys.stdout.buffer.write(b'<--- ' + line + b'\n')
                         (dictionaryOfWriters[sock]).send(dataFromSock)              # send the data to socks pair in the dictionary
                     else:
-                        for line in linesOfData:
-                            print("---> %s" % line)
-                        (dictionaryOfWriters[sock]).send(dataFromSock)              # send the data to socks pair in the dictionary
-
-
-
-
-
-
-
-
-
-
-
-
-
-    else:
-        print(errorString)
+                        if loggingOption == "-raw" or loggingOption == "-strip":
+                            for line in linesOfData:
+                                sys.stdout.buffer.write(b'---> ' + line + b'\n')
+                        (dictionaryOfClientWriters[sock]).send(dataFromSock)              # send the data to socks pair in the dictionary
