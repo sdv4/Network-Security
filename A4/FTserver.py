56ccb1fb33c61fe2d6df67af4dc8382ed954d395
shanesims <shane.sims.ss@gmail.com>
2017-11-08 22:12:41 -0700

    basic server funcitonality complete

--- A4/FTserver.py
+++ A4/FTserver.py
@@ -9,6 +9,7 @@
 from __future__ import print_function                                           # Import python3 print function
 import socket
 import sys
+import os.path
 
 #TODO: implement client authentication using shared key
 def authenticate_client(connection, key):
@@ -21,6 +22,7 @@ def serve(connection):
         connection.sendall("0")
         fileName = connection.recv(1024)                                        # Get name of file to be uploaded
         fileName = (str(fileName)).replace('\n','')                             # for connecting with netcat
+        print("Command: write   File name: " + fileName)
         theFile = open(fileName, "w+")                                          # Create file obj with specified name or write over file if already exists in dir
         connection.sendall(fileName)                                            # Echo file name back to client to indicate ready to receive
         while 1:                                                                # Receive file data from client until no more sent
@@ -30,12 +32,25 @@ def serve(connection):
                 break
         theFile.close()                                                         # Close/finish writting to file and
         connection.sendall("1")                                                 # Send success indicator
-        print("OK", file = sys.stderr)
         print("Status: success")
 
-    elif data is 1:                                                             # Client wants to download file
+    elif int(data) is 1:                                                             # Client wants to download file
         connection.sendall("1")
+        fileName = connection.recv(1024)                                        # Get name of file to be uploaded
+        fileName = (str(fileName)).replace('\n','')                             # for connecting with netcat
+        print("Command: read   File name: " + fileName)
+        if os.path.exists(fileName):
+            theFile = open(fileName, "r")
+            data = theFile.read()
+            fileSize = len(data)
+            connection.sendall(str(fileSize))                                        # send file size so client knows file exists and will come next
+            connection.sendall(data)                                            # Send file
+            theFile.close()
+            print("Status: success")
 
+        else:
+            connection.sendall("-1")                                            # File DNE, send protocol error code
+            print("Status: fail - " + fileName + " does not exist on server")
     else:
         connection.sendall("-1")                                                # Invalid argument from client, respond with error code
     connection.close()
@@ -53,14 +68,19 @@ def main():
         listening_socket.bind(("localhost",PORT))
         listening_socket.listen(5)                                                      # listen for client connections with queue for 5 connections
         print("FTserver started and listening on port " + str(PORT) + "\nUsing secret key: " + str(KEY))
-        while 1: #listen for client connections - serve - repeat
-            print("Waiting for client connection...")
-            client_connection, client_address = listening_socket.accept()
-            print("New connection from client on address " + str(client_address))
-            if authenticate_client(client_connection, KEY) :
-                serve(client_connection)
-            else:
-                print("Error: wrong key", file = sys.stderr)
+        try:
+            while 1: #listen for client connections - serve - repeat
+                print("Waiting for client connection...")
+                client_connection, client_address = listening_socket.accept()
+                print("New connection from client on address " + str(client_address))
+                if authenticate_client(client_connection, KEY) :
+                    serve(client_connection)
+                else:
+                    print("Error: wrong key", file = sys.stderr)
+        except KeyboardInterrupt:
+            print("\nControl-C detected. Closing server...")
+            listening_socket.close()
+            sys.exit(0)
     else:
         print("Error: wrong command line arguments", file = sys.stderr)
 
