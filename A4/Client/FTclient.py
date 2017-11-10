cfea39a6c61ac217b22df984ff9f98c51daebb5f
Mason-MacBook <MasonL@Masons-MacBook-Pro.local>
2017-11-10 13:56:57 -0700

    Starting crypto implementation

--- A4/Client/FTclient.py
+++ A4/Client/FTclient.py
@@ -7,12 +7,14 @@
 # Sources viewed or consulted:
 # https://docs.python.org/3/library/fileinput.html#fileinput.FileInput - for info on reading from stdin
 from __future__ import print_function                                           # Import python3 print function
-import socket
-import sys
-import fileinput
+import os, random, string, hashlib, socket, sys, fileinput
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
 
 def download(conn):
-    conn.sendall("1")                                                           # Indicate to server that client wants to download files
+    encrypted_bytes = doEncrypt(READ)
+    conn.sendall(encrypted_bytes)                                                           # Indicate to server that client wants to download files
     data = conn.recv(1024)
     if str(data) == "1":                                                        # Server ready to receive fileName
         conn.sendall(fileName)
@@ -33,7 +35,7 @@ def download(conn):
     sys.exit()
 
 def upload(conn):
-    conn.sendall("0")                                                           # Indicate to server that client wants to upload files
+    conn.sendall("write")                                                           # Indicate to server that client wants to upload files
     data = conn.recv(1024)
     if str(data) == "0":
         conn.sendall(fileName)
@@ -53,6 +55,56 @@ def upload(conn):
     conn.close()
     sys.exit()
 
+# Create session key using sha3-256(seed|nonce|"SK")
+def getSessionKey(nonce):
+    session_key = hashlib.sha256(KEY + nonce + "SK").digest()
+    if cipher == "aes128":
+        return session_key[:16]                     # Returns the first 16 bytes for AES-CBC-128
+    elif  cipher == "aes256":
+        return session_key                         # Returns all 32 bytes for AES-CBC-256
+
+# Create IV using sha3-256(seed|nonce|"IV")
+def getIV(nonce):
+    iv = hashlib.sha256(KEY + nonce + "IV").digest()
+    return iv[:16]              # Returns the first 16 bytes (ie. the correct size for an IV in AES-CBC)
+
+# Create encryptor and padder objects needed for AES-CBC
+def initEncryptor():
+        backend = default_backend()
+        cipher = Cipher(algorithms.AES(session_key), modes.CBC(iv), backend=backend)        # Creates Cipher object using AES-CBC encryption
+        encryptor = cipher.encryptor()
+        padder = padding.PKCS7(128).padder()                                                # Creates padding objects to pad using PKCS7, the argument determines what multiple the data needs to be in (eg. 128 bit blocks for AES-CBC)
+        return encryptor, padder
+
+# Create decryptor and unpadder objects needed for AES-CBC
+def initDecryptor():
+        backend = default_backend()
+        cipher = Cipher(algorithms.AES(session_key), modes.CBC(iv), backend=backend)        # Creates Cipher object using AES-CBC encryption
+        decryptor = cipher.decryptor()
+        unpadder = padding.PKCS7(128).unpadder()
+        return decryptor, unpadder
+
+def doEncrypt(plaintext):
+    # Initialize cipher objects
+    encryptor, padder = initEncryptor()
+    padded_bytes = padder.update(plaintext) + padder.finalize()
+    encrypted_bytes = encryptor.update(padded_bytes) + encryptor.finalize()
+    return encrypted_bytes
+
+def doDecrypt(ciphertext):
+    # Initialize cipher objects
+    decryptor, unpadder = initDecryptor()
+    decrypted_bytes = decryptor.update(ciphertext) + decryptor.finalize()
+    unpadded_bytes = unpadder.update(decrypted_bytes) + unpadder.finalize()
+    return unpadded_bytes
+
+# Verify the client's response to the challenge with the server's own digest
+def checkResponse(client_response, nonce, session_key):
+    server_response = hashlib.sha256(challenge + session_key).digest()
+    if server_response == client_response:
+        return True
+    else:
+        return False
 
 def main():
     global PORT
@@ -61,6 +113,12 @@ def main():
     global command
     global fileName
     global cipher
+    global session_key
+    global iv
+    global ACK
+    global READ
+    global WRITE
+    global ERROR
 
     if(len(sys.argv) == 6):
         command = str(sys.argv[1])
@@ -70,9 +128,24 @@ def main():
         PORT = int(hostPort[1])
         cipher = str(sys.argv[4])
         KEY  = str(sys.argv[5])
+        ACK = "1"
+        READ = "read"
+        WRITE = "write"
+        ERROR = "-1"
+
+        if cipher != "null":
+            # Initialize AES arguments
+            nonce = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(16)) # Concatenates 16 randomly chosen alphanumeric characters
+            session_key = getSessionKey(nonce)
+            iv = getIV(nonce)
 
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.connect((HOST, PORT))
+
+        #send the first message to server containing cipher and nonce
+        first_message = cipher + "," + nonce
+        server_socket.sendall(first_message)
+
         if command == "write":
             upload(server_socket)
 
