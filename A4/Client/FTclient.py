fe5d50f2d1cacf474160697b048049bb8084fd58
Mason-MacBook <MasonL@Masons-MacBook-Pro.local>
2017-11-11 00:42:35 -0700

    Masons final working copy. Fixed some merging issues from my last commit.

--- A4/Client/FTclient.py
+++ A4/Client/FTclient.py
@@ -36,6 +36,8 @@ def download(conn):
             print("Error: file could not be read by server", file = sys.stderr)
     else:
         print("Error: file could not be read by server", file = sys.stderr)
+    conn.close()
+    sys.exit()
 
 def upload(conn):
     sendData(conn, WRITE)                                                       # Indicate to server that client wants to upload files
@@ -181,9 +183,6 @@ def main():
 
     else:
         print("Error: wrong command line arguments", file = sys.stderr)
-
-    conn.close()
-    sys.exit()
 
 
 if __name__ == "__main__":
