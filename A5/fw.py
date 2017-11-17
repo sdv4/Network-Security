059cd55a3c3722fe9ec32d997f1641788157df51
Mason-MacBook <MasonL@Masons-MacBook-Pro.local>
2017-11-17 02:08:27 -0700

    Working version that filters packets. Needs testing. Also see comments for QUESTIONs and TODOs

--- A5/fw.py
+++ A5/fw.py
@@ -2,16 +2,17 @@ import sys, fileinput
 
 # Returns true if ip1 is in the CIDR block of ip2.
 # ie. ip1 and ip2 have the same prefix
+# OUTPUT: True if IPs are the same or part of the same CIDR block, False otherwise
 def checkCIDRPrefix(ip1, ip2):
     ip1Int = IPtoInt(ip1)
-    if "/" in ip2:
+    if "/" in ip2:                                                  # If a CIDR block was given as ip2, check that the prefix is the same between both IPs
         ip2, offset = ip2.split("/")
         ip2Int = IPtoInt(ip2)
         if maskIP(ip1Int, int(offset)) == maskIP(ip2Int, int(offset)):
             return True
         else:
             return False
-    else:
+    else:                                                           # Otherwise two IP addresses should be compared bit-by-bit
         ip2Int = IPtoInt(ip2)
         if ip1Int == ip2Int:
             return True
@@ -20,7 +21,8 @@ def checkCIDRPrefix(ip1, ip2):
 
 
 # Returns an ip with its lower bits masked by an amount of (32 - <offset>)
-# INPUT: int <ip>, int <number_of_higher_bits>
+# INPUT: Int <ip>, Int <number_of_higher_bits>
+# OUTPUT: Int <ip> - with masked bits
 def maskIP(ip, offset):
     lowerBits = 32 - offset
     try:
@@ -31,6 +33,7 @@ def maskIP(ip, offset):
     return ip
 
 # Convert IP in CIDR notation to an integer
+# OUTPUT: Int <ip>
 def IPtoInt(IPString):
     ip = IPString.split(".")
     try:
@@ -55,33 +58,34 @@ def readConfig(configFile):
         sys.exit(0)
     return
 
+# Filters packet based on the set of rules from the config file
+# OUTPUT: String <output> - what is done with each packet
 def filterPacket(packet):
-    output = DEFAULT
-    packet = packet.replace("\n", "")
     fields = packet.split()
-    if len(fields) != 4:
-        output += packet
-        return output + packet
-    rules = configDict.copy()
-    for key in list(rules.keys()):
+    rules = configDict.copy()                                       # Create copy of rules
+
+    for key in list(rules.keys()):                                  # Iterate through each rule from the config file dropping rules that do not apply to the packet
         if fields[0] != rules[key][0]:                              # Remove rule with a different direction than the packet's
             del rules[key]
-        elif (fields[2] not in list(rules[key][3].split(","))) and (rules[key][3] != "*"):    # Remove rule that applies to ports that are different than the packet's
+        elif (fields[2] not in list(rules[key][3].split(","))) and (rules[key][3] != "*"):    # Remove rule where the ports do not match
             del rules[key]
-        elif (fields[3] == "0") and (len(rules[key]) == 5) and (rules[key][4] == "established"): # Remove rule for established packets when packet part of a new session
+        elif (fields[3] == "0") and (len(rules[key]) == 5) and (rules[key][4] == "established"): # Remove rule if the packet is not established and the rule is for established packets
             del rules[key]
-        elif (rules[key][2] != "*") and not(checkCIDRPrefix(fields[1], rules[key][2])):       # Remove rule that applies to a block of different IPs
+        elif (rules[key][2] != "*") and not(checkCIDRPrefix(fields[1], rules[key][2])):       # Remove rule if the IP or CIDR block does not match the packet's IP
             del rules[key]
-    if len(rules) >= 1:
+
+    if len(rules) >= 1:                                             # Take the first rule left in the set of rules. TODO: Ask about multiple rules applying to one packet. Case: packets1.txt (packet 5), rules1.txt (rule 2 and 6)
         key, value = rules.popitem()
-        output = value[1] + "(" + str(key) + ")" + " " + packet
+        output = value[1] + "(" + str(key) + ")" + " " + ' '.join(fields)   #TODO: ' '.join(fields) makes it so that a '|' doesn't appear in the diff
+        #output = value[1] + "(" + str(key) + ")" + " " + packet            #TODO: packet makes it so that a '|' appears in the diff
         return output
-    elif len(rules) == 0:
-        output += packet
+    elif len(rules) == 0:                                           # Drop the package as the default option when no rules apply
+        output = DEFAULT + ' '.join(fields)
+        #output = DEFAULT + packet                                  #TODO: See a few lines above
         return output
     #TODO: QUESTION: If a packet is accepted under multiple rules, which takes precedence?
     # else:
-    #     print("Error: FILTERING IS WRONG. Rules left " + str(list(rules.keys())))
+    #     print("Error: Too many rules apply. Rules left " + str(list(rules.keys())))
     #     sys.exit(0)
 
 def main():
@@ -90,15 +94,11 @@ def main():
     if(len(sys.argv) == 2):
         configFile = str(sys.argv[1])
         configDict = {}
-        ACCEPT = "accept"
-        DROP = "drop"
-        REJECT = "reject"
         DEFAULT = "drop() "
         readConfig(configFile)
         packet = sys.stdin.readline()
         while (packet):
             output = filterPacket(packet)
-            #sys.stdout.write(output)
             print(output)
             packet = sys.stdin.readline()
     else:
